954a32dbefc0dbc3e859fa5e3639a8b90d0981cc
tomal2915 <tomal2915@gmail.com>
2025-08-01 22:21:29 +0600

    Add linting and formatting configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
-# ESLint config
-echo 'module.exports = {
+module.exports = {
   root: true,
   env: {
     node: true,
@@ -19,28 +18,4 @@ echo 'module.exports = {
   rules: {
     // Add custom rules here
   }
-}' > .eslintrc.js
-
-# Prettier config
-echo '{
-  "semi": true,
-  "singleQuote": true,
-  "printWidth": 100,
-  "tabWidth": 2,
-  "trailingComma": "es5",
-  "arrowParens": "avoid"
-}' > .prettierrc
-
-# EditorConfig
-echo 'root = true
-
-[*]
-indent_style = space
-indent_size = 2
-end_of_line = lf
-charset = utf-8
-trim_trailing_whitespace = true
-insert_final_newline = true
-
-[*.md]
-trim_trailing_whitespace = false' > .editorconfig
+};
